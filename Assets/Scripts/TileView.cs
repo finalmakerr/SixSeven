053ebce0fa95869d8b7605609665fd53dfe70774
finalmakerr <finalmakerr@gmail.com>
2026-02-03 13:58:41 +0200

    Improve match-3 input and shuffle

--- Assets/Scripts/TileView.cs
+++ Assets/Scripts/TileView.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TileView : MonoBehaviour
+public class TileView : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     [SerializeField] private Image image;
     [SerializeField] private GameObject selection;
     [SerializeField] private Button button;
+    [SerializeField] private float dragThreshold = 28f;
 
     private GameManager manager;
+    private RectTransform rectTransform;
+    private Vector2 pointerDownPosition;
+    private bool dragTriggered;
 
     public int X { get; private set; }
     public int Y { get; private set; }
@@ -19,6 +24,7 @@ public class TileView : MonoBehaviour
             image = GetComponent<Image>();
         if (button == null)
             button = GetComponent<Button>();
+        rectTransform = GetComponent<RectTransform>();
     }
 
     public void Init(GameManager owner, int x, int y)
@@ -30,7 +36,6 @@ public class TileView : MonoBehaviour
         if (button != null)
         {
             button.onClick.RemoveAllListeners();
-            button.onClick.AddListener(() => manager.OnTileClicked(this));
         }
     }
 
@@ -47,5 +52,44 @@ public class TileView : MonoBehaviour
     {
         if (selection != null)
             selection.SetActive(isSelected);
+    }
+
+    public void SetScale(float scale)
+    {
+        if (rectTransform == null)
+            rectTransform = GetComponent<RectTransform>();
+        if (rectTransform != null)
+            rectTransform.localScale = Vector3.one * scale;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDownPosition = eventData.position;
+        dragTriggered = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (dragTriggered || manager == null)
+            return;
+
+        Vector2 delta = eventData.position - pointerDownPosition;
+        if (delta.magnitude < dragThreshold)
+            return;
+
+        dragTriggered = true;
+        Vector2 direction = Mathf.Abs(delta.x) > Mathf.Abs(delta.y)
+            ? (delta.x > 0 ? Vector2.right : Vector2.left)
+            : (delta.y > 0 ? Vector2.up : Vector2.down);
+
+        manager.OnTileDragged(this, direction);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (dragTriggered || manager == null)
+            return;
+
+        manager.OnTileClicked(this);
     }
 }
