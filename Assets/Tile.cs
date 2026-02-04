81085ed4850fd101a95abc9dd8e87caf5410570f
finalmakerr <finalmakerr@gmail.com>
2026-02-04 09:40:37 +0200

    Refactor tile prefabs for candy-style grid

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -6,12 +6,29 @@ public class Tile : MonoBehaviour
 
     private void Awake()
     {
-        if (sr == null) sr = GetComponent<SpriteRenderer>();
+        if (sr == null)
+            sr = FindIconRenderer();
     }
 
     public void SetSprite(Sprite sprite)
     {
-        if (sr == null) sr = GetComponent<SpriteRenderer>();
+        if (sr == null)
+            sr = FindIconRenderer();
+        if (sr == null)
+            return;
+
         sr.sprite = sprite;
+    }
+
+    private SpriteRenderer FindIconRenderer()
+    {
+        Transform iconTransform = transform.Find("TileUI/Icon");
+        if (iconTransform == null)
+            iconTransform = transform.Find("Icon");
+
+        if (iconTransform != null)
+            return iconTransform.GetComponent<SpriteRenderer>();
+
+        return GetComponent<SpriteRenderer>();
     }
 }
