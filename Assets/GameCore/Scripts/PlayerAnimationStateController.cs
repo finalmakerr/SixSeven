39a6df7937f3012a7adb217b5b0c76bea32c5bca
finalmakerr <finalmakerr@gmail.com>
2026-02-06 10:39:09 +0200

    Add player animation state controller

---
+++ Assets/GameCore/Scripts/PlayerAnimationStateController.cs
@@ -0,0 +1,163 @@
+using UnityEngine;
+
+namespace GameCore
+{
+    public enum PlayerAnimationState
+    {
+        Idle = 0,
+        Happy = 1,
+        Excited = 2,
+        Stunned = 3,
+        Worried = 4,
+        Meditating = 5,
+        Shielded = 6,
+        Tired = 7
+    }
+
+    public class PlayerAnimationStateController : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Animator animator;
+
+        [Header("Game State Flags")]
+        [SerializeField] private bool isHappy;
+        [SerializeField] private bool isExcited;
+        [SerializeField] private bool isStunned;
+        [SerializeField] private bool isWorried;
+        [SerializeField] private bool isMeditating;
+        [SerializeField] private bool isShielded;
+        [SerializeField] private bool isTired;
+
+        public PlayerAnimationState CurrentState { get; private set; } = PlayerAnimationState.Idle;
+
+        private static readonly int StateParam = Animator.StringToHash("State");
+
+        public bool IsHappy
+        {
+            get => isHappy;
+            set => isHappy = value;
+        }
+
+        public bool IsExcited
+        {
+            get => isExcited;
+            set => isExcited = value;
+        }
+
+        public bool IsStunned
+        {
+            get => isStunned;
+            set => isStunned = value;
+        }
+
+        public bool IsWorried
+        {
+            get => isWorried;
+            set => isWorried = value;
+        }
+
+        public bool IsMeditating
+        {
+            get => isMeditating;
+            set => isMeditating = value;
+        }
+
+        public bool IsShielded
+        {
+            get => isShielded;
+            set => isShielded = value;
+        }
+
+        public bool IsTired
+        {
+            get => isTired;
+            set => isTired = value;
+        }
+
+        private void Reset()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Awake()
+        {
+            if (animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+        }
+
+        private void Update()
+        {
+            var nextState = ResolveStateFromFlags();
+            if (nextState == CurrentState)
+            {
+                return;
+            }
+
+            CurrentState = nextState;
+            if (animator != null)
+            {
+                animator.SetInteger(StateParam, (int)CurrentState);
+            }
+        }
+
+        public void SetStateFlags(
+            bool happy,
+            bool excited,
+            bool stunned,
+            bool worried,
+            bool meditating,
+            bool shielded,
+            bool tired)
+        {
+            isHappy = happy;
+            isExcited = excited;
+            isStunned = stunned;
+            isWorried = worried;
+            isMeditating = meditating;
+            isShielded = shielded;
+            isTired = tired;
+        }
+
+        private PlayerAnimationState ResolveStateFromFlags()
+        {
+            if (isStunned)
+            {
+                return PlayerAnimationState.Stunned;
+            }
+
+            if (isShielded)
+            {
+                return PlayerAnimationState.Shielded;
+            }
+
+            if (isMeditating)
+            {
+                return PlayerAnimationState.Meditating;
+            }
+
+            if (isExcited)
+            {
+                return PlayerAnimationState.Excited;
+            }
+
+            if (isHappy)
+            {
+                return PlayerAnimationState.Happy;
+            }
+
+            if (isWorried)
+            {
+                return PlayerAnimationState.Worried;
+            }
+
+            if (isTired)
+            {
+                return PlayerAnimationState.Tired;
+            }
+
+            return PlayerAnimationState.Idle;
+        }
+    }
+}
