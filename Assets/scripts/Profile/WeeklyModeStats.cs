24d901edc3bea0e4e72d8978b5993929d18b8eb8
finalmakerr <finalmakerr@gmail.com>
2026-02-16 23:42:17 +0200

    add weekly mode stats model

---
+++ Assets/scripts/Profile/WeeklyModeStats.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class WeeklyModeStats
+{
+    public int normalCompleted;
+    public int hardcoreCompleted;
+    public int ironmanCompleted;
+    public DateTime weekStartUtc;
+}
