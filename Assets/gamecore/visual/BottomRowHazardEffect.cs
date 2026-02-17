ff3f5503b040f74f682769fd2b3d4850b5ac4fa5
finalmakerr <finalmakerr@gmail.com>
2026-02-17 16:08:03 +0200

    add hazard transition feedback

--- Assets/gamecore/visual/BottomRowHazardEffect.cs
+++ Assets/gamecore/visual/BottomRowHazardEffect.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 namespace GameCore
 {
@@ -7,12 +8,34 @@ namespace GameCore
         [SerializeField] private GameObject poisonEffect;
         [SerializeField] private GameObject fireEffect;
         [SerializeField] private GameObject iceEffect;
+        private Coroutine pulseRoutine;
 
         public void SetHazard(HazardType type)
         {
             poisonEffect.SetActive(type == HazardType.Poison);
             fireEffect.SetActive(type == HazardType.Fire);
             iceEffect.SetActive(type == HazardType.Ice);
+        }
+
+        public void Pulse()
+        {
+            if (pulseRoutine != null)
+            {
+                StopCoroutine(pulseRoutine);
+            }
+
+            pulseRoutine = StartCoroutine(PulseRoutine());
+        }
+
+        private IEnumerator PulseRoutine()
+        {
+            float scaleUp = 1.05f;
+            Vector3 originalScale = transform.localScale;
+
+            transform.localScale = originalScale * scaleUp;
+            yield return new WaitForSeconds(0.2f);
+            transform.localScale = originalScale;
+            pulseRoutine = null;
         }
     }
 }
